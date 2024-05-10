9f858b4f890f9946acf6e310e2a67e765bbfd564
miacdelgado <153124560+miacdelgado@users.noreply.github.com>
2024-05-10 23:44:12 +0000

    Question C Finished

--- src/question_c/main.py
+++ src/question_c/main.py
@@ -1,1 +1,5 @@
-#add import
+from question_c import StockList
+
+if __name__ == "__main__":
+    stock_list = StockList()
+    stock_list.display_stock_info()
--- src/question_c/question_c.py
+++ src/question_c/question_c.py
@@ -1,1 +1,25 @@
-#write functions here, don't add input('') statements here!
+class Stock:
+    def __init__(self, symbol, company_name):
+        self.__symbol = symbol
+        self.__company_name = company_name
+
+    def get_symbol(self):
+        return self.__symbol
+
+    def get_company_name(self):
+        return self.__company_name
+
+class StockList:
+    def __init__(self):
+        self.stock_data = {
+            "AAPL": "Apple Inc",
+            "CAT": "Caterpillar",
+            "EK": "Eastman Kodak",
+            "GOOG": "Google",
+            "MSFT": "Microsoft"
+        }
+
+    def display_stock_info(self):
+        print("{:<10} {:<20}".format("Symbol", "Company Name"))
+        for symbol, company_name in self.stock_data.items():
+            print("{:<10} {:<20}".format(symbol, company_name))
